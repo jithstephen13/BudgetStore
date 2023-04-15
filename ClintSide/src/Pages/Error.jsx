52a30a19df131406cacd61d2dff340fb17c56f2d
jithstephen13 <jithstephen96@gmail.com>
2023-04-15 16:22:23 +0530

    add pages Index and Product and Single product

---
+++ ClintSide/src/Pages/Error.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const Error = () => {
+    const navigate=useNavigate()
+     
+  return (
+    <div className="container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-12">
+    <div className="wf-ull lg:w-1/2">
+        <p className="text-sm font-medium text-blue-500 dark:text-blue-400">404 error</p>
+        <h1 className="mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl">Page not found</h1>
+        <p className="mt-4 text-gray-500 dark:text-gray-400">Sorry, the page you are looking for doesn't exist.Here are some helpful links:</p>
+
+        <div className="flex items-center justify-center    mt-6 gap-x-3">
+           
+
+            <button onClick={()=>navigate("/")} className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                Take me home
+            </button>
+        </div>
+    </div>
+
+    <div className="relative w-full mt-12 lg:w-1/2 lg:mt-0">
+        <img className="w-full max-w-lg lg:mx-auto" src="https://merakiui.com/images/components/illustration.svg" alt="img"/>
+    </div>
+</div>
+  )
+}
+
+export default Error
+
+
---
+++ ClintSide/src/Pages/Product/Products.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react'
+import SideFilterBar from '../../Components/SideFilter/SideFilterBar'
+import { useSelector,useDispatch } from 'react-redux'
+import { getcategeryAsync } from '../../redux/catagery/CategrySlice'
+import { getproductAsync } from '../../redux/Product/productSlice'
+import ProductCard from '../../Components/ProductGridCard/ProductCard'
+import Scleton from '../../Components/Loading/Scleton'
+import SlideUp from '../../Components/SlideUp'
+import SideFilterBarSmall from '../../Components/SideFilter/SideSmolScreen'
+import TransitionEfect from '../../Components/TransitionEffectes/TransitionEfect'
+
+const Products = () => {
+  const [filter,setFilter]=useState(null)
+ const dispatch= useDispatch()
+ let path=window.location.pathname
+
+ path=path.slice(1,path.length)
+ const [Slide,setSlideUp]=useState(false)
+
+ const {categries}=useSelector((store)=>store.categries)
+ const {products,loading}=useSelector((store)=>store.products)
+
+
+
+ useEffect(()=>{
+    dispatch(getcategeryAsync(path))
+   
+     
+ },[])
+
+
+ 
+ useEffect(()=>{
+
+  if(categries.length!==0&&categries[2].filterName!=undefined){
+ dispatch(getproductAsync({categry:path,filterSlu:categries[0].filterName}))
+  }
+ 
+ },[categries])
+
+
+
+
+
+
+
+
+  return (
+    <>
+    < TransitionEfect />
+    <div className=' relative flex mt-16 w-full pt-2 justify-between '  >
+    
+       <SideFilterBar/>
+       <div onClick={()=>setSlideUp(!Slide)}  className=' md:hidden  absolute w-16 h-16 z-10 top-1 left-1   flex items-center justify-center rounded-2xl bg-yellow-300'>
+               <h1>{Slide ?"Hide ":"Filter Menu"}</h1>
+       </div>
+       {Slide&& <SlideUp >
+           <div className='absolute top-16  w-36 h-auto z-30 rounded-xl overflow-hidden'>
+              <SideFilterBarSmall/>
+           </div>
+          
+         </SlideUp>}
+
+<div className="p-4   sticky top-6 w-full md:ml-10 transition delay-150 duration-300 ease-in-out "> 
+{loading && <Scleton/>}
+   <div className=" grid grid-cols-1  md:grid-cols-2 gap-4  pl-4">
+      
+               {/* product Section */}
+              
+
+               {products.products&&products.products.length>=1&&products.products.map((item,idx)=>{
+
+               return <ProductCard key={idx} product={item}/>
+
+               })}
+
+
+   </div>
+</div>
+</div>
+</> )
+}
+
+export default Products
